bb0af817319c2006fb98fb234ea2b9e6aabf28a1
Alisson <alissonrodrigo1996@gmail.com>
2023-07-21 18:02:42 -0300

    segundo commit

---
+++ App.js
@@ -0,0 +1,26 @@
+async function pegarTexto (){
+    primeiroTexto = document.getElementById('#') 
+    segundoTexto = document.getElementById('#')
+    terceiroTexto = documento.getElementById('#')
+
+    pegarTexto();
+}
+
+async function downloadBotao (){
+    document.getElementsByClassName('botao')
+}
+
+async function teste (){
+    const testeTexto = "#"
+    const testeInputTexto = "#"
+}
+
+
+async function inputTexto (){
+    const arrTextoSalvo = ['#']
+
+}
+
+async function checkInput () {
+    const possivelCheck = ['#']   
+}
